f073d1d6eedeaff1bbb3807d236b20760e480464
GABO <gabrielvazquezcivica@gmail.com>
2026-05-06 03:23:47 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const config = {
+
+    // 🤖 Nombre del bot
+    botName: 'SPIDER BOT',
+
+    // 👑 Nombre del dueño principal
+    ownerName: 'SoyGabo',
+
+    // 📱 Owners del bot (puedes agregar varios)
+    owner: [
+        '13652994839', // principal
+        '18252500344', // owner 2
+    ],
+
+    // ⚙️ Prefijo
+    prefix: '.'
+
+}
+
+export default config
