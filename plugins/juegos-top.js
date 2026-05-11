cf5b53f663d882a0d74bd1bd683d831f4f291be3
GABO <gabrielvazquezcivica@gmail.com>
2026-05-10 18:48:41 -0600

    Create juegos-top.js

---
+++ plugins/juegos-top.js
@@ -0,0 +1,101 @@
+import fs from 'fs'
+
+const path = './data/modoadmin.json'
+
+// 📥 DB
+function getDB() {
+    try {
+
+        if (!fs.existsSync(path))
+            return {}
+
+        return JSON.parse(
+            fs.readFileSync(path, 'utf-8')
+        )
+
+    } catch {
+
+        return {}
+    }
+}
+
+const handler = async ({
+    sock,
+    m,
+    from,
+    isGroup,
+    participants,
+    args,
+    sender
+}) => {
+
+    if (!isGroup) return
+
+    const db = getDB()
+    const isBlockedGroup = db[from]
+
+    // 🔐 ADMIN
+    const user = participants.find(
+        p => p.id === sender
+    )
+
+    const isAdmin =
+        user?.admin === 'admin' ||
+        user?.admin === 'superadmin'
+
+    // 🔥 MODODADMIN
+    if (isBlockedGroup && !isAdmin)
+        return
+
+    const tema = args.join(' ')
+
+    if (!tema) {
+
+        return sock.sendMessage(from,{
+            text:
+`⚠️ Usa el comando así:
+
+.top infieles
+.top pajeros
+.top chismosos
+.top dormidos`
+        },{ quoted:m })
+    }
+
+    // 🔥 RANDOM 10
+    const randomUsers = participants
+        .sort(() => Math.random() - 0.5)
+        .slice(0, 10)
+
+    const mentions =
+        randomUsers.map(u => u.id)
+
+    let text =
+`╭━━━〔 🕷️ SPIDER TOP 〕━━━⬣
+┃
+┃ 🏆 TOP ${tema.toUpperCase()}
+┃
+`
+
+    randomUsers.forEach((u, i) => {
+
+        text +=
+`┃ ${i + 1}. @${u.id.split('@')[0]}
+`
+    })
+
+    text +=
+'╰━━━━━━━━━━━━━━━━⬣'
+
+    await sock.sendMessage(from,{
+        text,
+        mentions
+    },{ quoted:m })
+}
+
+handler.command = ['top']
+handler.tags = ['juegos']
+handler.group = true
+handler.menu = true
+
+export default handler
