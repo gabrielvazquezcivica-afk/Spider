3e1442a3ea25cb3b34a2398812afaacc4b2327e9
GABO <gabrielvazquezcivica@gmail.com>
2026-06-16 08:12:24 -0600

    Crear juegos-gemidos.js

---
+++ plugins/juegos-gemidos.js
@@ -0,0 +1,119 @@
+import fs from 'fs'
+
+const audios = [
+
+'https://files.catbox.moe/xxxxx.mp3',
+'https://files.catbox.moe/yyyyy.mp3',
+'https://files.catbox.moe/zzzzz.mp3',
+'https://files.catbox.moe/aaaaa.mp3'
+
+]
+
+const handler = async ({
+sock,
+m,
+from,
+participants,
+sender
+}) => {
+
+// 🔒 MODODADMIN
+let isBlockedGroup = false
+
+try {
+
+    const db = JSON.parse(
+        fs.readFileSync(
+            './data/modoadmin.json'
+        )
+    )
+
+    isBlockedGroup = db[from]
+
+} catch {}
+
+const user = participants?.find(
+    p => p.id === sender
+)
+
+const isAdmin =
+    user?.admin === 'admin' ||
+    user?.admin === 'superadmin'
+
+if (
+    isBlockedGroup &&
+    !isAdmin
+) return
+
+try {
+
+    const audio =
+        audios[
+            Math.floor(
+                Math.random() *
+                audios.length
+            )
+        ]
+
+    await sock.sendMessage(
+        from,
+        {
+            react:{
+                text:'🎵',
+                key:m.key
+            }
+        }
+    )
+
+    await sock.sendMessage(
+        from,
+        {
+            audio:{
+                url: audio
+            },
+            mimetype:'audio/mpeg',
+            ptt:true
+        },
+        {
+            quoted:m
+        }
+    )
+
+    await sock.sendMessage(
+        from,
+        {
+            react:{
+                text:'✅',
+                key:m.key
+            }
+        }
+    )
+
+} catch (e) {
+
+    console.log(
+        'GEM ERROR:',
+        e
+    )
+
+    await sock.sendMessage(
+        from,
+        {
+            text:
+
+'❌ Error enviando audio.'
+},
+{
+quoted:m
+}
+)
+}
+}
+
+handler.command = ['gemidos']
+handler.tags = ['juegos']
+handler.help = ['gem']
+handler.group = true
+handler.menu = true
+
+export default handler
